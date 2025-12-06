5774e04ee604a0a784e8a021c19f3a4f1b0a4f12
Fuad Shurkuzade <fuad.shukurzadeh@gmail.com>
2025-12-06 11:07:07 +0400

    initial commit

---
+++ src/main/java/com/Fuad/BankApplicationSimulation/BankApplicationSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.Fuad.BankApplicationSimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplicationSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplicationSimulationApplication.class, args);
+	}
+
+}
