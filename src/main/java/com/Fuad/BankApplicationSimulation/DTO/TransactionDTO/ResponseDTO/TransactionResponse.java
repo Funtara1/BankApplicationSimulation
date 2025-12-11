ca5c6998f28458bfccc15ac182924e7069965df5
Fuad Shurkuzade <fuad.shukurzadeh@gmail.com>
2025-12-12 01:47:37 +0400

    Add @JsonFormat to timestamp for custom date output in TransactionResponse

--- src/main/java/com/Fuad/BankApplicationSimulation/DTO/TransactionDTO/ResponseDTO/TransactionResponse.java
+++ src/main/java/com/Fuad/BankApplicationSimulation/DTO/TransactionDTO/ResponseDTO/TransactionResponse.java
@@ -1,5 +1,6 @@
-package com.Fuad.BankApplicationSimulation.DTO.TransactionDTO.Request.ResponseDTO;
+package com.Fuad.BankApplicationSimulation.DTO.TransactionDTO.ResponseDTO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 @Data
 public class TransactionResponse {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime date;
     private String type;
     private BigDecimal amount;
