558925755146d3b97eb576b9faf5cc21095791b0
Fuad Shurkuzade <fuad.shukurzadeh@gmail.com>
2025-12-14 17:38:38 +0400

    feat: add account existence check and pessimistic lock queries in AccountRepository

--- src/main/java/com/Fuad/BankApplicationSimulation/Repository/AccountRepository.java
+++ src/main/java/com/Fuad/BankApplicationSimulation/Repository/AccountRepository.java
@@ -1,7 +1,32 @@
 package com.Fuad.BankApplicationSimulation.Repository;
 
 import com.Fuad.BankApplicationSimulation.Entity.Account;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
+
+import jakarta.persistence.LockModeType;
+import java.util.List;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    // TODO: Remove not used
+    boolean existsByAccountNumber(String accountNumber);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({
+            @QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")
+    })
+    @Query("select a from Account a where a.id = :id")
+    Optional<Account> findByIdForUpdate(@Param("id") Long id);
+
+
+    // Для transfer (два аккаунта)
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select a from Account a where a.id in :ids")
+    List<Account> findAllByIdForUpdate(@Param("ids") List<Long> ids);
 }
