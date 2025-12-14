91af233526b4ce6da2017e657c57801f3d35c683
Fuad Shurkuzade <fuad.shukurzadeh@gmail.com>
2025-12-14 17:40:20 +0400

    refactor: add closeAccount and closeCustomer methods to services, remove deleteCustomerById

--- src/main/java/com/Fuad/BankApplicationSimulation/Service/CustomerService.java
+++ src/main/java/com/Fuad/BankApplicationSimulation/Service/CustomerService.java
@@ -1,15 +1,19 @@
-    package com.Fuad.BankApplicationSimulation.Service;
+package com.Fuad.BankApplicationSimulation.Service;
 
-    import com.Fuad.BankApplicationSimulation.DTO.CustomerDTO.RequestDTO.CreateCustomerRequest;
-    import com.Fuad.BankApplicationSimulation.Entity.Customer;
+import com.Fuad.BankApplicationSimulation.DTO.CustomerDTO.RequestDTO.CreateCustomerRequest;
+import com.Fuad.BankApplicationSimulation.Entity.Customer;
 
-    import java.util.List;
+import java.util.List;
 
-    public interface CustomerService {
-        Customer getCustomerById(Long id);
-        Customer createCustomer(CreateCustomerRequest createCustomerRequest);
-        Customer updateCustomerById(Long id, CreateCustomerRequest createCustomerRequest);
-        String deleteCustomerById(Long id);
-        List<Customer> getAllCustomers();
-    }
+public interface CustomerService {
 
+    Customer getCustomerById(Long id);
+
+    Customer createCustomer(CreateCustomerRequest createCustomerRequest);
+
+    Customer updateCustomerById(Long id, CreateCustomerRequest createCustomerRequest);
+
+    Customer closeCustomerById(Long id);
+
+    List<Customer> getAllCustomers();
+}
--- src/main/java/com/Fuad/BankApplicationSimulation/Service/TransactionService.java
+++ src/main/java/com/Fuad/BankApplicationSimulation/Service/TransactionService.java
@@ -7,14 +7,9 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface TransactionService {
-
     Transaction deposit(Long toAccountId, BigDecimal amount);
-
     Transaction withdraw(Long fromAccountId, BigDecimal amount);
-
     Transaction transfer(Long fromAccountId, Long toAccountId, BigDecimal amount);
-
     List<TransactionResponse> getTransactionsByAccountId(Long accountId);
-
     Transaction getTransactionById(Long id);
 }
