50076c8dbc13f5ddcf8b15fdda4b46fbc04d8f6e
Fuad Shurkuzade <fuad.shukurzadeh@gmail.com>
2025-12-14 17:23:18 +0400

    feat: add validation to deposit, transfer and withdraw requests

--- src/main/java/com/Fuad/BankApplicationSimulation/DTO/TransactionDTO/RequestDTO/DepositRequest.java
+++ src/main/java/com/Fuad/BankApplicationSimulation/DTO/TransactionDTO/RequestDTO/DepositRequest.java
@@ -1,5 +1,7 @@
 package com.Fuad.BankApplicationSimulation.DTO.TransactionDTO.RequestDTO;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +10,11 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class DepositRequest {
+
+    @NotNull
     private Long toAccountId;
+
+    @NotNull
+    @Positive
     private BigDecimal amount;
 }
--- src/main/java/com/Fuad/BankApplicationSimulation/DTO/TransactionDTO/RequestDTO/TransferRequest.java
+++ src/main/java/com/Fuad/BankApplicationSimulation/DTO/TransactionDTO/RequestDTO/TransferRequest.java
@@ -1,5 +1,7 @@
 package com.Fuad.BankApplicationSimulation.DTO.TransactionDTO.RequestDTO;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +10,14 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class TransferRequest {
+
+    @NotNull
     private Long fromAccountId;
+
+    @NotNull
     private Long toAccountId;
+
+    @NotNull
+    @Positive
     private BigDecimal amount;
 }
