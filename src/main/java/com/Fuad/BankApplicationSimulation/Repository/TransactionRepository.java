840861ebcd5544c19be95e2aaaf6afa47a2f0bda
Fuad Shurkuzade <fuad.shukurzadeh@gmail.com>
2025-12-14 17:39:26 +0400

    refactor: replace custom query with derived method for TransactionRepository to sort by timestamp

--- src/main/java/com/Fuad/BankApplicationSimulation/Repository/TransactionRepository.java
+++ src/main/java/com/Fuad/BankApplicationSimulation/Repository/TransactionRepository.java
@@ -2,16 +2,20 @@ package com.Fuad.BankApplicationSimulation.Repository;
 
 import com.Fuad.BankApplicationSimulation.Entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    @Query("""
-        select t from Transaction t 
-        where t.fromAccount.id = :accountId
-           or t.toAccount.id = :accountId
-    """)
-    List<Transaction> findByAccountId(Long accountId);
+
+//    @Query("""
+// select t from Transaction t
+// where (t.fromAccount.id = :accountId or t.toAccount.id = :accountId)
+// order by t.timestamp desc
+//""")
+//    List<Transaction> findByAccountId(Long accountId);
+
+    List<Transaction> findAllByFromAccountIdOrToAccountIdOrderByTimestampDesc(Long fromAccountId, Long toAccountId);
+
+
 }
