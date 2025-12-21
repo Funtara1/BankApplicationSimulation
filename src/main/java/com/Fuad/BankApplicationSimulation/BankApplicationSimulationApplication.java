12d92b6ce079fb8980466412cb8528a983fe6ce1
Fuad Shurkuzade <fuad.shukurzadeh@gmail.com>
2025-12-22 01:01:09 +0400

    build: switch from Maven to Gradle

--- src/main/java/com/Fuad/BankApplicationSimulation/BankApplicationSimulationApplication.java
+++ src/main/java/com/Fuad/BankApplicationSimulation/BankApplicationSimulationApplication.java
@@ -2,12 +2,13 @@ package com.Fuad.BankApplicationSimulation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BankApplicationSimulationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplicationSimulationApplication.class, args);
 	}
-
 }
