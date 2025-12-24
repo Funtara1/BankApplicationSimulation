de57542dc85ef056bd2ef09eae85ca2d93c329d2
Fuad Shurkuzade <fuad.shukurzadeh@gmail.com>
2025-12-24 13:59:54 +0400

    refactor: update duplicate exception code

--- src/main/java/com/fuad/bank/domain/exception/DuplicateException.java
+++ src/main/java/com/fuad/bank/domain/exception/DuplicateException.java
@@ -1,11 +1,10 @@
 package com.fuad.bank.domain.exception;
 
 import org.springframework.http.HttpStatus;
-
 import java.util.Map;
 
 public class DuplicateException extends AppException {
     public DuplicateException(String message, Map<String, Object> details) {
-        super("DUPLICATE_ACCOUNT", HttpStatus.CONFLICT, message, details);
+        super("DUPLICATE", HttpStatus.CONFLICT, message, details);
     }
 }
